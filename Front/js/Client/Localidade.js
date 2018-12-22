0ceddf5b6361da731056a76b0d0926d5d5cb22b8
DavidDutra <devide016@gmail.com>
2018-12-21 22:35:46 -0300

    .

--- Front/js/Client/Localidade.js
+++ Front/js/Client/Localidade.js
@@ -1,30 +1,62 @@
 function preencherDadosLocalidade() {
     $("#nomeEvento").empty();
+    $("#nomeLocalidade").empty();
+    $("#descLocalidade").empty();
+
     var json = servidor + "/Secult/localidade/listarLocalidadeComEvento";
 
     var onSuccess = function (result) {
 
         dados = result.localidades;
 
-        for(var i in dados){
+        for (var i in dados) {
 
             var idLocal = dados[i].id;
             var nomeEvento = dados[i].nomeEvento;
             var nome = dados[i].nome;
             var descricao = dados[i].descricao;
 
-            if($("#idLocalidade").val() == idLocal) {
+            if ($("#idLocalidade").val() == idLocal) {
                 $("#nomeLocalidade").empty();
                 $("#descLocalidade").empty();
                 $("#nomeLocalidade").append(nome);
                 $("#descLocalidade").append(descricao);
 
-                $("#nomeEvento").append("<ion-item style='border: none' class=\"item-icon-right item assertive\">"+nomeEvento+"\n" +
+
+                $("#nomeEvento").append("<ion-item style='border: none' class=\"item-icon-right item assertive\">" + nomeEvento + "\n" +
                     "        <i class=\"icon ion-android-alert\"></i>\n" +
                     "      </ion-item>");
             }
         }
 
+        setTimeout(function () {
+            if ($("#nomeEvento ion-item").length == 0) {
+                carregarNomeEdescEvento($("#idLocalidade").val());
+                $("#nomeEvento").append("<p style='font-size: 15px; color: red; text-align: center'>Não há nenhum evento previsto para esse mês!</p>");
+            }
+        }, 500);
+    }
+    $.getJSON(json, onSuccess).fail();
+}
+
+function carregarNomeEdescEvento(id) {
+    var json = servidor + "/Secult/localidade/listarLocalidade";
+
+    var onSuccess = function (result) {
+
+        var dados = result.localidades;
+
+        for (var i in dados) {
+
+            var idLocal = dados[i].id;
+            var local = dados[i].nome;
+            var descricao = dados[i].descricao;
+
+            if (id == idLocal) {
+                $("#nomeLocalidade").append(local);
+                $("#descLocalidade").append(descricao);
+            }
+        }
     }
     $.getJSON(json, onSuccess).fail();
 }
@@ -48,7 +80,7 @@ function selectLocalidade() {
             $("#idLocalidade option[value=1]").prop("selected", true).change()
         }
         $.getJSON(json, onSuccess).fail();
-    },1000)
+    }, 1000)
 }
 
 function selectLocalidadeCadastro() {
@@ -70,8 +102,9 @@ function selectLocalidadeCadastro() {
             $("#localidadeAdm option[value=1]").prop("selected", true).change()
         }
         $.getJSON(json, onSuccess).fail();
-    },1000)
+    }, 1000)
 }
+
 function selectLocalidadeUp() {
     setTimeout(function () {
 
@@ -91,5 +124,5 @@ function selectLocalidadeUp() {
             $("#localidadeUp option[value=1]").prop("selected", true).change()
         }
         $.getJSON(json, onSuccess).fail();
-    },1000)
+    }, 1000)
 }
